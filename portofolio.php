a37787fcb531c2813d335eb9fe7735cb6869aee2
mikailmloch <mikail0789@gmail.com>
2023-05-23 14:57:50 +0700

    menambahkan maps pada tiap footer setiap page, dan mengubah css agar semua gambar ukurannya seragam

--- portofolio.php
+++ portofolio.php
@@ -447,6 +447,10 @@
                         <a class="btn btn-outline-light btn-social" href="https://goo.gl/maps/AmmiJU78oZJ1s2fm6?coh=178571&entry=tt"><i class="fa-solid fa-map-location-dot"></i></a>
                     </div>
                 </div>
+                <div class="col-lg-3 col-md-6">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.323379323329!2d107.4911578!3d-6.8517856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e548be0c69c7%3A0x214a85c31194c7b6!2sKonveksi%20Cecep!5e0!3m2!1sid!2sid!4v1684753736364!5m2!1sid!2sid"
+                            width="400" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
         </div>
         <div class="container">
